0455da49bff0ac6f5d0069c48e98b45732f6f724
Weirdwagon <slendernet2@gmail.com>
2023-11-06 17:52:48 +0300

    inherited svg's

--- adv-app/src/components/home-page/HomePage.tsx
+++ adv-app/src/components/home-page/HomePage.tsx
@@ -5,6 +5,14 @@ import './HomePage.scss';
 import { Button } from "../buttons/button/Button";
 import { LocalizationProps } from "../../models/localization-props.type";
 import { ButtonStyle } from "../../models/button-style.enum";
+import { ReactComponent as CrownLong} from "../../logos/animated/crown-long.svg";
+import { ReactComponent as Crown} from "../../logos/animated/crown.svg";
+import { ReactComponent as DollarLong} from "../../logos/animated/dollar-long.svg";
+import { ReactComponent as M} from "../../logos/animated/m.svg";
+import { ReactComponent as Shield} from "../../logos/animated/shield.svg";
+import { ReactComponent as Soccer} from "../../logos/animated/soccer.svg";
+import { ReactComponent as Timer} from "../../logos/animated/timer.svg";
+import { ReactComponent as WorldLong} from "../../logos/animated/world-long.svg";
 
 export const HomePage: FC<LocalizationProps> = ({localizationContent}) => {
     const blog = useSelector(blogSelectors.blogEntitySelectors.selectAll);
@@ -17,6 +25,17 @@ export const HomePage: FC<LocalizationProps> = ({localizationContent}) => {
                 <h1  className="home-page-headings_element lower-heading">{localizationContent.get("project_name_body_h1_btm")}</h1>
                 <h2  className="home-page-headings_element sub-heading">{localizationContent.get("project_name_body_h2")}</h2>
                 <Button localizationContent={localizationContent.get("become_a_partner_body_btn") as string} buttonStyle={ButtonStyle.LightCapital} />
+            </div>
+            <div className="home-page-logos"> 
+                <div className="home-page-logos_container">
+
+                </div>
+                <div className="home-page-logos_container">
+
+                </div>
+                <div className="home-page-logos_container">
+
+                </div>
             </div>
             <div className="home-page-blog">
                 <h1 className="home-page-blog_heading">{localizationContent.get("blog_body_h1")}</h1>
